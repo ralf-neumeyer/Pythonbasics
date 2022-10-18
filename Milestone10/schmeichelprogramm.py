7320cf4a0b3f9fc1d1b5ac67f0d8b80a4c1b27c2
vitam <vitaminbevents@icloud.com>
2022-10-18 20:53:25 +0200

    erster Push tset

--- Milestone10/schmeichelprogramm.py
+++ Milestone10/schmeichelprogramm.py
@@ -1,1 +1,21 @@
-#Dies wird ein Schmeichelprogramm
+import random
+
+namen = ['ralf' , 'dieter' , 'klaus' , 'hans']
+
+def schmeichel():
+    adjektiv = ['traurig' , 'freudig' , 'betrunken' , 'herzlich']
+    norm = [ 'mensch' , 'hecht' , 'freund' , 'klumpel' , 'wannabe Programieren' ]
+    out = "Du bist der" + " " + (random.choice(adjektiv)) + " " + (random.choice(norm)) + " !!!"
+    return out
+
+rueck = schmeichel()
+print (rueck)
+for name in namen :
+    rueck = schmeichel()
+    print (name + " " + schmeichel())
+
+
+
+
+
+
